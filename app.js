3d608fb70607137e4683fe29e7d44c431fb6537f
KAMRUL HASSAN ZIBON <kzibonbd@gmail.com>
2023-06-18 23:51:58 +0600

    Relase

---
+++ app.js
@@ -0,0 +1,85 @@
+const http = require('http');
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
+const multiparty = require('multiparty');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/merge' && req.method === 'POST') {
+    const form = new multiparty.Form();
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error parsing form data');
+        return;
+      }
+
+      const imageFile = files.image[0];
+
+      if (!imageFile) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('No image file uploaded');
+        return;
+      }
+
+      const imagePath = `public/${imageFile.originalFilename}`;
+
+      fs.rename(imageFile.path, imagePath, (err) => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error uploading the image');
+          return;
+        }
+
+        Promise.all([
+          loadImage(imagePath),
+          loadImage('public/watermark.png')
+        ])
+          .then(([image, watermark]) => {
+            const canvas = createCanvas(image.width, image.height);
+            const ctx = canvas.getContext('2d');
+
+            ctx.drawImage(image, 0, 0);
+
+            const watermarkX = (image.width - watermark.width) / 2; // Center horizontally
+            const watermarkY = 20; // 20px padding from the top
+            ctx.drawImage(watermark, watermarkX, watermarkY);
+
+            const mergedImageURL = canvas.toDataURL('image/jpeg');
+
+            res.setHeader('Content-Disposition', 'attachment; filename=merged_image.jpg');
+            res.setHeader('Content-Type', 'image/jpeg');
+
+            const stream = canvas.createJPEGStream();
+            stream.pipe(res);
+          })
+          .catch((err) => {
+            console.error(err);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error processing the image');
+          });
+      });
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
